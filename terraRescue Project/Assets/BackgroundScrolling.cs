7e05860835e2e15434be9ee6a543ab261486cb89
notCandace <dorseymakenzie@yahoo.com>
2020-01-19 03:09:40 -0800

    SCDENES

---
+++ terraRescue Project/Assets/BackgroundScrolling.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScrolling : MonoBehaviour
+{
+
+    Material material;
+    Vector2 offset;
+    public float speed = 0.1f;
+
+    public int xVelocity, yVelocity;
+
+
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    void Start()
+    {
+
+        offset = new Vector2(xVelocity, yVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        material.mainTextureOffset += offset * Time.deltaTime * speed;
+
+    }
+}
