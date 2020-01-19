20d9d082e22151edcd310ce85f5c7c0315ef13e0
notCandace <dorseymakenzie@yahoo.com>
2020-01-19 10:39:19 -0800

    whats going on

--- terraRescue Project/Assets/mouseFollow.cs
+++ terraRescue Project/Assets/mouseFollow.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class mouseFollow : MonoBehaviour
 {
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class mouseFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(1))
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        }
     }
 }
