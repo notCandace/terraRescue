5d8da5e04e29468000d29fa0740446743c50a0d4
notCandace <dorseymakenzie@yahoo.com>
2020-01-19 11:13:10 -0800

    movement achieved (kinda)

--- terraRescue Project/Assets/Gameplay/Scripts/collisions.cs
+++ terraRescue Project/Assets/Gameplay/Scripts/collisions.cs
@@ -1,22 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class collisions : MonoBehaviour
 {
     public GameObject player;
+    public string levelName;
+    public int score = 0;
+    public Text text;
 
     public float speed = 0.15f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        text.text = "Score: 0";
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(-1, 0, 0) * speed;
+        //transform.position += new Vector3(-1, 0, 0) * speed;
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -27,11 +32,32 @@ public class collisions : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        else
+        else if (other.gameObject.tag == "grapple")
         {
-            player.SetActive(false);
+            levelName = "Legislature Scene";
+            SceneManager.LoadScene(levelName);
+            //change scene here to end credits????
+
+        }
+        else if (other.gameObject.tag == "under da sea")
+        {
+            levelName = "Ocean Scene";
+            SceneManager.LoadScene(levelName);
+            //change scene to marine level
+        }
+        else if (other.gameObject.tag == "down with the cows")
+        {
+            levelName = "Cattle Scene";
+            SceneManager.LoadScene(levelName);
+            //change scene to cattle level
+        }
+        else if (other.gameObject.tag == "collect")
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            text.text = "Score " + score;
         }
 
-            
+
     }
 }
