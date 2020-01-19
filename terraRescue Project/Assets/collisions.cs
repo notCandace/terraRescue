770af4aedc8ac0bc69bde0fecd6426dcb588a6a1
notCandace <dorseymakenzie@yahoo.com>
2020-01-18 17:57:01 -0800

    commiting 6 is better than 6000

---
+++ terraRescue Project/Assets/collisions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisions : MonoBehaviour
+{
+    public GameObject player;
+
+    public float speed = 0.15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(-1, 0, 0) * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("hit");
+
+        if (other.gameObject.tag == "death")
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            player.SetActive(false);
+        }
+
+            
+    }
+}
